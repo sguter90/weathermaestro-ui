1660c23395b8afa62eb7dff5be7fb14370b90e00
Christoph Müller <christoph@flying-lama.com>
2026-02-15 16:34:13 +0100

    adds missing env variables

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 # Replace environment variables in config.js
 envsubst '${API_BASE_URL}' < /usr/share/nginx/html/config.js.template > /usr/share/nginx/html/config.js
+envsubst '${APP_NAME}' < /usr/share/nginx/html/config.js.template > /usr/share/nginx/html/config.js
+envsubst '${APP_DESCRIPTION}' < /usr/share/nginx/html/config.js.template > /usr/share/nginx/html/config.js
 
 # Execute the original Nginx command
 exec "$@"
